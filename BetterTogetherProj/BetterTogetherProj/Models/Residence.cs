379cf4fdc6f431011a87da161a2dbaf699578279
doronbatsir <doronb.907@gmail.com>
2021-03-07 16:10:34 +0200

    Residence services

--- BetterTogetherProj/BetterTogetherProj/Models/Residence.cs
+++ BetterTogetherProj/BetterTogetherProj/Models/Residence.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BetterTogetherProj.Models.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -34,46 +35,10 @@ namespace BetterTogetherProj.Models
         public double Y { get => y; set => y = value; }
 
         public List<Residence> Read()
-        { 
-            List<Residence> ResidenceList = new List<Residence>();
-            List<string> NamesList = new List<string>();
-            List<double> YList = new List<double>();
-            List<double> XList = new List<double>();
-            using (XmlReader reader = XmlReader.Create("C:\\Users\\User\\Documents\\GitHub\\ProjectFinal\\BetterTogetherProj\\BetterTogetherProj\\App_Data\\XMLResidence.xml"))
-            {
-                while (reader.Read())
-                {
-                    if (reader.IsStartElement())
-                    {
-                        if (reader.Name.ToString() == "Value")
-                        {
-                            NamesList.Add(reader.ReadString());
-                        }
-                        if (reader.Name.ToString() == "Y")
-                        {
-                            YList.Add(Convert.ToDouble(reader.ReadString()));
-                        }
-                        if (reader.Name.ToString() == "X")
-                        {
-                            XList.Add(Convert.ToDouble(reader.ReadString()));
-                        }
-                    }   
-                }
-
-                int s = 1;
-                for (int t = 4; t < NamesList.Count; t=t+9)
-                {
-                    Residence r = new Residence();
-                    r.Id = s;
-                    r.Name = NamesList[t];
-                    r.Y = YList[s - 1];
-                    r.X = XList[s - 1];
-                    ResidenceList.Add(r);
-                    s++;
-                }
-                var newList = ResidenceList.OrderBy(x => x.Name).ToList();
-                return newList;
-            }
+        {
+                XMLServices xml = new XMLServices();
+                List<Residence> ResidenceList = xml.GetAllResidence();
+                return ResidenceList;  
         }
     }
 }
