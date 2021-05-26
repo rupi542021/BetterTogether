e7f3f18b75f390c1a6f0b88230e2057f1fbe2caf
yuviBA <77380780+yuviBA@users.noreply.github.com>
2021-05-26 15:17:45 +0300

    add studLocation class

---
+++ BetterTogetherProj/BetterTogetherProj/Models/StudLocation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterTogetherProj.Models
+{
+    public class StudLocation
+    {
+        string mail;
+        float x;
+        float y;
+
+        public StudLocation(string mail, float x, float y)
+        {
+            Mail = mail;
+            X = x;
+            Y = y;
+        }
+
+        public string Mail { get => mail; set => mail = value; }
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
+
+        public StudLocation()
+        {
+
+        }
+    }
+}
