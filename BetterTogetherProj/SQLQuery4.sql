4a3ebc16aad1f66fd68f20ad416a1a6f7e2b917c
Nessy janah <Nessyjanah94@gmail.com>
2021-06-13 21:54:42 +0300

    MORE FIX

--- BetterTogetherProj/SQLQuery4.sql
+++ BetterTogetherProj/SQLQuery4.sql
@@ -1,4 +1,4 @@
 ﻿select * from questionnaire_P3 where doplicateMode=1
-delete from questionnaire_P3 where doplicateMode=1
+delete from questionnaire_P3 where qrCode=706 or qrCode=705
 select * from question_P3
-delete from question_P3 where qrCode=701 and qCode=3
+delete from question_P3 where qrCode=705 and qCode=2
